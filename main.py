4b9e30eea3fda93fdc1c2b8dc526bb0d19d87367
Максим Райковский <maksim.raikovskyi@gmail.com>
2024-08-04 15:52:23 +0300

    Добавил условия на конструкционные ограничения

--- main.py
+++ main.py
@@ -10,18 +10,48 @@ class parametrs:
         self.L2 = L2
         self.L3 = L3
 
+    def coordinate_test(self):
+
+        if np.sqrt(self.x**2 + self.y**2) > self.L1 +self.L2 + self.L3:
+
+            print( f'Error x = {self.x} and y= {self.y} out of range' )
+
+            return False
+
+        return True
+
     def calc_ungle(self):
 
-        theta_1 = np.arccos( (self.L2**2 - self.L1**2 - self.y**2 - (self.x - self.L3)**2) /
+        if self.coordinate_test():
+
+            theta_1 = np.arccos( (self.L2**2 - self.L1**2 - self.y**2 - (self.x - self.L3)**2) /
                              (2 * self.L1 * np.sqrt( self.y**2 +(self.x - self.L3)**2 ) ) ) - np.arctan( self.y / (self.x - self.L3) )
 
-        theta_2 = np.arccos( (self.y**2 +(self.x - self.L3)**2 - self.L2**2 - self.L1**2) / 2. / self.L1 / self.L2 )
+            theta_2 = np.arccos( (self.y**2 +(self.x - self.L3)**2 - self.L2**2 - self.L1**2) / 2. / self.L1 / self.L2 )
 
-        theta_3 = theta_1 + theta_2
+            theta_3 = theta_1 + theta_2
 
-        return theta_1 / np.pi * 180., theta_2 / np.pi * 180., theta_3 / np.pi * 180.
+            if theta_1 >= 3. * np.pi / 2. or theta_1 < 0.0:
 
-ungle1 = parametrs(3., 4., 3.2, 2., 0.2)
+                print( f'Error theta_1 = {theta_1 / np.pi * 180.} out of range' )
+
+            if theta_2 < 0.0 or theta_2 >= 3. * np.pi / 4.:
+
+                print( f'Error theta_2 = {theta_2 / np.pi * 180.} out of range' )
+
+            if theta_3 < 0.0 or theta_3 >= 5. * np.pi / 4.:
+
+                print( f'Error theta_2 = {theta_3 / np.pi * 180.} out of range' )
+
+        #return theta_1, theta_2, theta_3
+
+            return theta_1 / np.pi * 180., theta_2 / np.pi * 180., theta_3 / np.pi * 180.
+
+        else:
+
+            return False
+
+ungle1 = parametrs(2., 4., 3.2, 2., 0.2)
 
 print(ungle1.calc_ungle())
 
